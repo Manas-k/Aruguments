bcf31d71af1b949067af25e1eb9c052f1f8ba8fc
Manas-k <74486519+Manas-k@users.noreply.github.com>
2020-11-27 18:22:39 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -40,16 +40,3 @@ function draw() {
   }
   drawSprites();
 }
-
-function isTouching(o1,o2){
-  if (o1.x - o2.x < o2.width/2 + o1.width/2
-    && o2.x - o1.x < o2.width/2 + o1.width/2
-    && o1.y - o2.y < o2.height/2 + o1.height/2
-    && o2.y - o1.y < o2.height/2 + o1.height/2) {
-  return true;
-}
-else {  
-  return false;
-}
-
-}
